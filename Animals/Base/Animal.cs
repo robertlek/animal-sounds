fda06f3400637688a6b4f7191a7c0cc150a96e77
robertlek <lecarobertandrei@gmail.com>
2024-03-15 08:37:37 +0200

    Added a list of animals and looped through each one in order to call the 'MakeSound' method from the base class.

--- Animals/Base/Animal.cs
+++ Animals/Base/Animal.cs
@@ -1,11 +1,11 @@
 ﻿namespace animal_sounds.Animals.Base;
 
-public class Animal(string name, string sound)
+public abstract class Animal(string name, string sound)
 {
     protected readonly string name = name;
     protected readonly string sound = sound;
 
-    protected void MakeSound()
+    public void MakeSound()
     {
         Console.WriteLine($"{name} makes the sound *{sound}*.");
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using animal_sounds.Animals;
+using animal_sounds.Animals.Base;
+
+List<Animal> animals =
+[
+    new Cat("Cat", "meow"),
+    new Chicken("Chicken", "cluck"),
+    new Dog("Dog", "bark"),
+    new Horse("Horse", "neigh"),
+    new Sheep("Sheep", "bleat")
+];
+
+foreach (var animal in animals)
+{
+    animal.MakeSound();
+}
